812078f187ffc68f207f48ffd9926c8f2a601458
pavlav <pa.lavrentyev@gmail.com>
2024-02-06 13:30:21 +0300

    Add comments commit

--- app/src/main/java/com/lavrent/businesscardapp/MainActivity.kt
+++ app/src/main/java/com/lavrent/businesscardapp/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
@@ -37,7 +40,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingPreview()
+                    GreetingPreview() // Функция предпоказа, в ней сама составная функция находится, решил просто так написать
                 }
             }
         }
@@ -45,18 +48,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CardOne() {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .padding(
-                top = 340.dp,
-                bottom = 100.dp,
-                start = 20.dp,
-                end = 20.dp
-            )
+fun CardOne() { // Первая карточка
+    Column( // Столбец карточки
+        horizontalAlignment = Alignment.CenterHorizontally // Выравнивание по середине в горизонте
     ) {
-        Box(Modifier
+        Box(Modifier // Создал контейнер для отображения отступов в котором находится иконка, можно опустить этот момент и здать картинкку по высоте иширине
             .padding(
                 top = 10.dp,
                 bottom = 10.dp,
@@ -65,101 +61,130 @@ fun CardOne() {
             )
         ) {
             Icon(
-                painter = painterResource(id = R.drawable.baseline_face_retouching_natural_black_48),
-                contentDescription = null,
+                painter = painterResource(id = R.drawable.baseline_face_retouching_natural_black_48), // Иконка лица находится в ресурсах
+                contentDescription = null, // Описание рисунка или иконки , обязательный параметр, можно его опустить, поставить null
                 Modifier
-                    .size(
+                    .size( // Модификатор размер иконки
                         height = 120.dp,
                         width = 200.dp
-                )
+                ),
+                tint = Color.Blue // Оттенок иконки
             )
         }
-        Text(
-            text = stringResource(R.string.lavrentyev_pavel),
-            fontSize = 40.sp,
-            fontWeight = FontWeight.Bold,
-            fontStyle = FontStyle.Italic
+        Text( // Текст первый карточки
+            text = stringResource(R.string.lavrentyev_pavel), // Текст описан в ресурсах string
+            fontSize = 40.sp, // Размер шрифта
+            fontWeight = FontWeight.Bold, // Жирный шрифт
+            fontStyle = FontStyle.Italic, // Стиль шрифта
+            color = Color.Black // Цвет текста
         )
-        Text(
-            text = stringResource(R.string.android_developer),
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Green
+        Text( // Текст второй карточки
+            text = stringResource(R.string.android_developer), // Текст описан в ресурсах string
+            fontSize = 20.sp, // Размер шрифта
+            fontWeight = FontWeight.Bold, // Жирный шрифт
+            color = Color.Green // Цвет текста
         )
     }
 }
 
 @Composable
-fun CardTwo(modifier: Modifier = Modifier) {
-    Column(
+fun CardTwo(modifier: Modifier = Modifier) { // Вторая карточка
+    Column( // Столбец
         Modifier
-            .padding(
-                top = 10.dp,
-                bottom = 20.dp,
-                start = 20.dp,
-                end = 20.dp
-            )
+            .height(190.dp), // Высота всего контейнера столбца
+        verticalArrangement = Arrangement.Bottom // Выравнивание вниз по вертикали
     ) {
-        Row(modifier.padding(10.dp)) {
-            Column(
+        Row(modifier // Первый ряд
+            .padding( // Отступы контенера ряд
+                top = 10.dp,
+                bottom = 10.dp,
+                start = 40.dp,
+                end = 40.dp
+            )
+            .height(35.dp) // Высота ряда
+        ) {
+            Column( // Контейнер стобца(по сути, можно было взять обычный Box), нужен указть вес в ряду, отступы относительно элемента рядом в ряду
                 Modifier
                     .weight(1f)
                     .padding(end = 10.dp),
-                horizontalAlignment = Alignment.End
+                horizontalAlignment = Alignment.End // Элементы или элемент занимает по горизонтали крайнее правое положение отностльно контенера и его отступов
             ) {
                 Icon(
                     painter = painterResource(id = R.drawable.baseline_call_black_48),
-                    contentDescription = null
+                    contentDescription = null, // Описание рисунка или иконки , обязательный параметр, можно его опустить, поставить null
+                    tint = Color.DarkGray // Оттенок иконки
                 )
             }
             Column(
-                Modifier.weight(2f)
+                Modifier.weight(2f) // Вес контейнера относильно ряда
             ) {
-                Text(
-                    text = stringResource(R.string.tel),
-                    fontSize = 20.sp
+                Text( // Первый текст карточки 2
+                    text = stringResource(R.string.tel), // Текст описан в ресурсах string
+                    fontSize = 20.sp, // Размер шрифта
+                    color = Color.Black // Цвет текста
                 )
             }
         }
-        Row(modifier.padding(10.dp)) {
-            Column(
+        Row(modifier  // Второй ряд
+            .padding( // Отступы контенера ряд
+                top = 10.dp,
+                bottom = 10.dp,
+                start = 40.dp,
+                end = 40.dp
+            )
+            .height(35.dp) // Высота ряда
+        ) {
+            Column( // Контейнер стобца(по сути, можно было взять обычный Box), нужен указть вес в ряду, отступы относительно элемента рядом в ряду
                 Modifier
                     .weight(1f)
                     .padding(end = 10.dp),
-                horizontalAlignment = Alignment.End
+                horizontalAlignment = Alignment.End // Элементы или элемент занимает по горизонтали крайнее правое положение отностльно контенера и его отступов
             ) {
                 Icon(
                     painter = painterResource(id = R.drawable.android_logo),
-                    contentDescription = null
+                    contentDescription = null, // Описание рисунка или иконки , обязательный параметр, можно его опустить, поставить null
+                    tint = Color.Green, // Оттенок иконки
+
                 )
             }
             Column(
-                Modifier.weight(2f)
+                Modifier.weight(2f) // Вес контейнера относильно ряда
             ) {
-                Text(
-                    text = stringResource(R.string.https_github_com_gipnozhard),
-                    fontSize = 14.sp
+                Text( // Второй текст карточки 2
+                    text = stringResource(R.string.https_github_com_gipnozhard), // Текст описан в ресурсах string
+                    fontSize = 18.sp, // Размер шрифта
+                    color = Color.Black // Цвет текста
                 )
             }
         }
-        Row(modifier.padding(10.dp)) {
-            Column(
+        Row(modifier // Третий ряд
+            .padding( // Отступы контенера ряд
+                top = 10.dp,
+                bottom = 10.dp,
+                start = 40.dp,
+                end = 40.dp
+            )
+            .height(35.dp) // Высота ряда
+        ) {
+            Column( // Контейнер стобца(по сути, можно было взять обычный Box), нужен указть вес в ряду, отступы относительно элемента рядом в ряду
                 Modifier
                     .weight(1f)
                     .padding(end = 10.dp),
-                horizontalAlignment = Alignment.End
+                horizontalAlignment = Alignment.End // Элементы или элемент занимает по горизонтали крайнее правое положение отностльно контенера и его отступов
             ) {
                 Icon(
                     painter = painterResource(id = R.drawable.baseline_email_black_48),
-                    contentDescription = null
+                    contentDescription = null, // Описание рисунка или иконки , обязательный параметр, можно его опустить, поставить null
+                    tint = Color.Magenta // Оттенок иконки
                 )
             }
             Column(
-                Modifier.weight(2f)
+                Modifier.weight(2f) // Вес контейнера относильно ряда
             ) {
-                Text(
-                    text = stringResource(R.string.pa_lavrentyev_gmail_com),
-                    fontSize = 18.sp
+                Text( // Третий текст карточки 2
+                    text = stringResource(R.string.pa_lavrentyev_gmail_com), // Текст описан в ресурсах string
+                    fontSize = 17.sp, // Размер шрифта
+                    color = Color.Black // Цвет текста
                 )
             }
         }
@@ -167,10 +192,15 @@ fun CardTwo(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun Greeting() {
-    Column(Modifier.background(Color(0xFF8F9779))) {
-        CardOne()
-        CardTwo(Modifier.weight(1f))
+fun Greeting() { // Общая составная функция в Compose
+    Column( // Контейнер столбец
+        Modifier
+            .background(Color(0xFF8F9779)), // Цвет фона
+        verticalArrangement = Arrangement.Bottom // Вертикально выравнивание элементов контейнера по нижнему краю
+    ) {
+        CardOne() // Карточка один
+        Spacer(modifier = Modifier.height(100.dp)) // Функция пропуска, пробела с высотой 100 пикселей
+        CardTwo() // Карточка два
     }
 }
 
@@ -178,6 +208,6 @@ fun Greeting() {
 @Composable
 fun GreetingPreview() {
     BusinessCardAppTheme {
-        Greeting()
+        Greeting() //Составная функция предтавленная в функции представления(предпоказа)
     }
 }
